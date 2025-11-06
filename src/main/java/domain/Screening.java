cf22b5233cd64cdf23beec104fd35966623766b6
mike7643 <developerJYumin@gmail.com>
2025-11-06 21:22:23 +0900

    feat(domain): 상영(Screening) 클래스 정의 (Movie + Place + Time 조합)

---
+++ src/main/java/domain/Screening.java
@@ -0,0 +1,35 @@
+package domain;
+
+import java.time.LocalDateTime;
+
+public class Screening {
+
+    private final Movie movie;
+    private final Place place;
+    private final LocalDateTime startDayTime;
+
+    public Screening(Movie movie, Place place, LocalDateTime startDayTime) {
+        this.movie = movie;
+        this.place = place;
+        this.startDayTime = startDayTime;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+    public Place getPlace() {
+        return place;
+    }
+    public LocalDateTime getStartDayTime() {
+        return startDayTime;
+    }
+
+    boolean isMovieDay(){
+        int dayOfMonth = startDayTime.getDayOfMonth();
+        return dayOfMonth == 10 || dayOfMonth == 20 || dayOfMonth == 30;
+    }
+
+    boolean isDiscountTime(){
+        return startDayTime.getHour() < 11 || startDayTime.getHour() >= 20;
+    }
+}
