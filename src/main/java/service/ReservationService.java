3314830eacc747faa373b2b79825c12e9d4421e0
mike7643 <developerJYumin@gmail.com>
2025-11-08 12:51:18 +0900

    feat(service): 예매(Reservation) 생성 비즈니스 로직 통합
        (시간 겹침 검증 -> 좌석 중복 검증 -> PriceCalculator 가격 계산 -> Reservation 객체 생성 및 저장)

--- src/main/java/service/ReservationService.java
+++ src/main/java/service/ReservationService.java
@@ -6,6 +6,7 @@ import domain.Screening;
 import domain.Seat;
 import domain.User;
 import repository.ReservationRepository;
+import validate.ReservedSeatValidator;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,15 +15,39 @@ import java.util.Optional;
 public class ReservationService {
 
     private final ReservationRepository reservationRepository;
+    private final ReservedSeatValidator reservedSeatValidator;
+    private final PriceCalculator priceCalculator;
 
-    public ReservationService(ReservationRepository reservationRepository) {
+
+    public ReservationService(ReservationRepository reservationRepository,
+                              ReservedSeatValidator reservedSeatValidator,
+                              PriceCalculator priceCalculator) {
         this.reservationRepository = reservationRepository;
+        this.reservedSeatValidator = reservedSeatValidator;
+        this.priceCalculator = priceCalculator;
     }
 
 
     public Reservation createReservation(User user, Screening screening, List<Seat> selectedSeats, long usePoint, Payment payment) {
-        // TODO: 좌석 검증 -> 가격 계산 -> 포인트 차감 -> 객체 생성 -> 저장
-        throw new UnsupportedOperationException("아직 구현되지 않았습니다.");
+
+        reservedSeatValidator.validate(screening, selectedSeats);
+        int finalPrice = priceCalculator.calculatePrice(
+                screening, selectedSeats, usePoint, payment
+        );
+
+        user.usePoint(usePoint);
+
+
+        Reservation reservation = new Reservation(
+                user,
+                screening,
+                selectedSeats,
+                payment,
+                usePoint,
+                finalPrice
+        );
+
+        return reservationRepository.save(reservation);
     }
 
     public Optional<Reservation> findReservationById(Long reservationId) {
